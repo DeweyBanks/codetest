ac740c694265cf963e9ebd344c7db2f487f5e8d7
Dewey Banks <DeweyBanks@gmail.com>
2017-03-03 18:01:15 -0500

    adds order test

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -10,6 +10,16 @@ RSpec.describe Order, type: :model do
     should have_many(:shipments)
   end
 
+  it "should assign shipment to warehouse based on product" do
+    product = Product.find_by(name: 'tablet')
+    warehouse = product.inventory.warehouse
+    order = Order.new
+    order.products << product
+    order.save
+    shipment = Shipment.find_by(order_id: order.id)
+    expect(shipment.warehouse).to eql(warehouse)
+  end
+
   it { is_expected.to callback(:create_shipment).after(:create) }
   it { is_expected.to callback(:assign_number).after(:create) }
 end
